e53799db091f3778154ed00ca0178bdeba076399
Vishnu Anand <VishnuAnand@users.noreply.github.com>
2017-12-28 16:21:28 +0530

    Add files via upload

---
+++ src/project1/bin/NewClass.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1.bin;
+
+/**
+ *
+ * @author user
+ */
+public class NewClass {
+    if (s == 1) {
+
+                        status = 0;
+                        if (coin == ba && bas == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bas = 1;
+                        } else if (coin == ba && bas == 1) {
+
+                        }
+
+                        if (coin == bb && bbs == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bbs = 1;
+                        } else if (coin == bb && bbs == 1) {
+
+                        }
+
+                        if (coin == bc && bcs == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bcs = 1;
+                        } else if (coin == bc && bcs == 1) {
+
+                        }
+
+                        if (coin == bd && bds == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bds = 1;
+                        } else if (coin == bd && bds == 1) {
+                        }
+                        player = 2;
+                    }
+                    if (s == 4) {
+                        status = 0;
+                        if (coin == ba && bas == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bas = 1;
+                        } else if (coin == ba && bas == 1) {
+                        }
+
+                        if (coin == bb && bbs == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bbs = 1;
+                        } else if (coin == bb && bbs == 1) {
+
+                        }
+
+                        if (coin == bc && bcs == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bcs = 1;
+                        } else if (coin == bc && bcs == 1) {
+                        }
+
+                        if (coin == bd && bds == 0) {
+                            coin.move(x, y);
+                            //s=0;
+                            bds = 1;
+                        } else if (coin == bd && bds == 1) {
+                        }
+                        player = 2;
+                    }
+}
