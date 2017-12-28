00e68e55787e10ce28f18d6da00fb111f299b11a
Vishnu Anand <VishnuAnand@users.noreply.github.com>
2017-12-28 16:22:11 +0530

    Add files via upload

---
+++ src/project1/Randam.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+/**
+ *
+ * @author user
+ */
+class Randam {
+
+    int nextInt(int maximum) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
